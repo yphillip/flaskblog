6524d87b46f04cf4b03aea38437f4bc4ee028f7e
Phillip Yen <yphillip@gmail.com>
2019-11-08 17:32:21 -0800

    fleshed out register form

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8dede169966fc9d7b089c693582318e4'
 
 posts = [
     {
@@ -25,6 +29,15 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":

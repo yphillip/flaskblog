e95cfd889c36d57c4441cbfcef479bf8be843bae
Phillip Yen <yphillip@gmail.com>
2019-11-10 08:05:00 -0800

    finished Part 3 - Forms and User Input

--- forms.py
+++ forms.py
@@ -11,7 +11,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[
-                                         DataRequired(), EqualTo(password)
+                                         DataRequired(), EqualTo('password')
                                      ])
     submit = SubmitField('Sign up')
 

0905981553947ca3aea291737531fd7ea55f2a14
Phillip Yen <yphillip@gmail.com>
2019-11-28 15:52:13 -0800

    finished Part 11 - Blueprints and Configuration

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flaskblog import db, app
+from flask import current_app
+from flaskblog import db
 from flaskblog import login_manager
 from flask_login import UserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -19,12 +20,12 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(secret_key=app.config['SECRET_KEY'], expires_in=expires_sec)
+        s = Serializer(secret_key=current_app.config['SECRET_KEY'], expires_in=expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(secret_key=app.config['SECRET_KEY'])
+        s = Serializer(secret_key=current_app.config['SECRET_KEY'])
         try:
             user_id = s.loads(token)['user_id']
         except:

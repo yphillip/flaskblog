bd1b8ffc4fe92fe91354f6ec4f810176e18cc3b8
Phillip Yen <yphillip@gmail.com>
2019-11-30 18:27:36 -0800

    edited DEBUG_VALUE

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 from flaskblog import create_app
 
-DEBUG_VALUE = os.environ.get('DEBUG_VALUE')
+DEBUG_VALUE = (os.environ.get('DEBUG_VALUE') == "True")
 
 app = create_app()
 if __name__ == "__main__":

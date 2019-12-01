68d7a28010c6388c26ca04bba2918e998b4e5458
Phillip Yen <yphillip@gmail.com>
2019-11-30 19:19:41 -0800

    migrated database, I think

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 from flaskblog.config import Config
 
 db = SQLAlchemy()
@@ -18,6 +19,7 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate = Migrate(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
